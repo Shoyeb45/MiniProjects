4f430ed765f8e44e90534d2463bb38778ebebfd2
Shoyeb45 <shoyebff45@gmail.com>
2024-03-03 23:07:49 +0530

    completed tic tac toe

--- Tic Tac Toe/script.js
+++ Tic Tac Toe/script.js
@@ -1,7 +1,12 @@
+// Accessing all necessary html elements
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#resetBtn");
+let newGameBtn = document.querySelector("#newGameBtn");
+let msg = document.querySelector("#msg");
+let msgContainer = document.querySelector(".msgContainer");
 
 let turnO = false;
+
 // Winning pattern
 const winPattern = [
     [0, 1, 2],
@@ -14,23 +19,54 @@ const winPattern = [
     [6, 7, 8]
 ];
 
+// Event listener for clicking button
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         // condition for turns
         if(turnO) {
             turnO = false;
             box.innerText = "O";
+            box.style.color = "red";
         } else {
             turnO = true;
             box.innerText = "X";
+            box.style.color = "aqua";
         }
         box.disabled = true;
         checkWinner();
     })
 })
 
+const resetGame = () => {
+    turnO = true;
+    enableBtn();
+    msgContainer.classList.add("hide");
+}
+
+// Function for disabling the button after winner is found
+const disableBtn = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+}
+
+// Function for enabling the button after reset button is tapped
+const enableBtn = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+// Function for checking the winner
+const showWinner = (winner) => {
+    msg.innerText = `Congratulation, the winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBtn();
+}
 
 // function for checking the winning pattern
+let count = 0;
 const checkWinner = () => {
     for(let pattern of winPattern) {
         let valPos1 = boxes[pattern[0]].innerText;
@@ -39,8 +75,19 @@ const checkWinner = () => {
 
         if(valPos1 != "" && valPos2 != "" && valPos3 != "") {
             if(valPos1 === valPos2 && valPos2 === valPos3) {
-                alert("Winner");
+                showWinner(valPos1);
             }
         }
     }
+    count++;
+    if(count === 9) {
+        msg.innerText = "Game is draw";
+        msgContainer.classList.remove("hide");
+        disableBtn();
+    }
 }
+
+// Events for reset button and new game
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
