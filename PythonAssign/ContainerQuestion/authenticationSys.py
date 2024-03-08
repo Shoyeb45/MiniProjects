befec13ffa735d1a027d688b1ca5a8ccf5390c69
Shoyeb45 <shoyebff45@gmail.com>
2024-03-08 11:03:58 +0530

    PyAssig - 2 : All conditions written , main code remaining

---
+++ PythonAssign/ContainerQuestion/authenticationSys.py
@@ -0,0 +1,81 @@
+# length criteria
+def pswrdLen(s):
+    if(len(s) <= 10):
+        return True
+    return False
+
+# number of uppercase and lowercase criteria
+def pswrdCase(s):
+    upperCase, lowerCase = 0, 0
+    for i in range(0, len(s)):
+        if(s[i] >= 'A' and s[i] <= 'Z'):
+            upperCase += 1
+        else:
+            lowerCase += 1
+
+    if(upperCase >= 2 and lowerCase >= 2):
+        return True
+    return False
+
+# Digits Criteria
+def pswdDigit(s):
+    cnt = 0
+    for i in range(0, len(s)):
+        if(s[i] >= '0' and s[i] <= '9'):
+            cnt += 1
+
+    if(cnt >= 2):
+        return True
+    return False
+    
+# Special char criteria 
+def pswdSpecialChar(s):
+    cnt = 0
+    for i in range(0, len(s)):
+        if(s[i] == '@' or s[i] == '#' or s[i] == '$' or s[i] == '%' or s[i] == '&' or s[i] == '*' or s[i] == '!'):
+            cnt += 1
+
+    if(cnt >= 2):
+        return True
+    return False
+
+# repeatation criteria
+def pswdCharRepeat(s):
+    i = 0
+    while(i <= len(s)-1):
+        cnt = 1
+        for j in range(i+1, len(s)):
+            if s[i] == s[j]:
+                cnt += 1
+            else:
+                break
+        if  cnt > 3:
+            return False
+        i += cnt
+        
+    return True
+
+# Username consecutive repeatation
+def pswdUserName(username, s):
+    i = 0
+    while(i <= len(username)-1):
+        for j in range(0, len(s)-1):
+            if username[i] == s[j]:
+                cnt = 1
+                for k in range(j+1, len(s)):
+                    if s[j] == s[k]:
+                        cnt += 1
+                    else:
+                        break
+                
+                if cnt > 3:
+                    return False
+        i += 1
+
+    return True
+        
+
+# Main code
+username = input("Enter user name:")
+
+                
