436fb82b02ae037c418c6c714d72509cfe5d3f8e
Shoyeb45 <shoyebff45@gmail.com>
2024-03-09 03:10:37 +0530

    Secure Authentication System

--- PythonAssign/ContainerQuestion/authenticationSys.py
+++ PythonAssign/ContainerQuestion/authenticationSys.py
@@ -1,6 +1,6 @@
 # length criteria
 def pswrdLen(s):
-    if(len(s) <= 10):
+    if(len(s) >= 10):
         return True
     return False
 
@@ -32,7 +32,7 @@ def pswdDigit(s):
 def pswdSpecialChar(s):
     cnt = 0
     for i in range(0, len(s)):
-        if(s[i] == '@' or s[i] == '#' or s[i] == '$' or s[i] == '%' or s[i] == '&' or s[i] == '*' or s[i] == '!'):
+        if((s[i] >= '!' and s[i] <= '/') or (s[i] >= ':' and s[i] <= '@')):
             cnt += 1
 
     if(cnt >= 2):
@@ -77,5 +77,60 @@ def pswdUserName(username, s):
 
 # Main code
 username = input("Enter user name:")
+pswdList = []
+while True:
+    ans = True
+    pswd = input("Enter password:")
 
-                
+    length = pswrdLen(pswd)
+    case = pswrdCase(pswd)
+    digit = pswdDigit(pswd)
+    specChar = pswdSpecialChar(pswd)
+    charRep = pswdCharRepeat(pswd)
+    charUser = pswdUserName(username, pswd)
+
+    if len(pswdList) >= 3:
+        if pswdList[0] == pswd and pswdList[1] == pswd and pswdList[2] == pswd:
+            ans = False
+        pswdList.pop(0)
+
+    if ans:
+        if length:
+            ans = ans and length
+            if case:
+                ans = ans and case
+                if digit:
+                    ans = ans and digit
+                    if specChar:
+                        ans = ans and specChar
+                        if charRep:
+                            ans = charRep and specChar
+                            if charUser:
+                                ans = ans and charUser
+                            else:
+                                ans = False
+                                print("The password should not contain any sequence of three or more consecutive characters from the username")
+                        else:
+                            ans = False
+                            print("Your password has characters that is repeating more than three times in a row")
+                    else:
+                        ans = False
+                        print("Please enter atleast 2 special characters(@, #, $, %, &, *, !).")
+                else:
+                    ans = False
+                    print("Please enter atleast 2 digits.")
+            else:
+                ans = False
+                print("Please enter atleast 2 uppercase and lowercase letter.")
+        else:
+            ans = False
+            print("Please enter atleast 10 characters.")
+    else:
+        print("Your password is same as that of last three password")
+
+
+
+    if(ans):
+        print("Your password is valid.")
+        break
+    pswdList.append(pswd)            
